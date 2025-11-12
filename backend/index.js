8176cdbae656b0b0170a01ac587fff661f515b93
Fernando Bedoya <juanfer30082000@gmail.com>
2025-11-11 19:33:33 -0500

    emails templates

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -11,19 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to parse incoming requests: req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
-const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`✅ Server running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error("❌ Failed to connect to MongoDB:", error.message);
-        process.exit(1);
-    }
-};
 
-startServer();
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`✅ Server running on port ${PORT}`);
+});
--- backend/mail/emails.js
+++ backend/mail/emails.js
@@ -1,4 +1,4 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplate.js";
+import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplate.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -17,5 +17,63 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     } catch (error) {
         console.log(`Error sending verification:`, error);
         throw new Error(`Error sending verification email: ${error.message}`);
+    }
+}
+
+export const sendWelcomeEmail = async (email, name) => {
+
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "ca568ab5-e495-4549-855d-c1928621b6a0",
+            template_variables: {
+                company_info_name: "Cuarto Pixel",
+                name: name
+            },
+        })
+
+        console.log("Email sent successfully:", response);
+    } catch (error) {
+        console.log(`Error sending verification:`, error);
+        throw new Error(`Error sending verification email: ${error}`);
+    }
+}
+
+export const sendPasswordResetEmail = async (email, resetLink) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Reset your password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetLink),
+            category: "Password Reset"
+        })
+    } catch (error) {
+        console.log(`Error sending password reset email:`, error);
+        throw new Error(`Error sending password reset email: ${error}`);
+    }
+}
+
+export const sendResetSuccessEmail = async (email) => {
+    const recipient = [{ email }]
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Password reset successful",
+            html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+            category: "Password Reset"
+        })
+
+        console.log("Email sent successfully:", response);
+    } catch (error) {
+        console.log(`Error sending password reset email:`, error);
+        throw new Error(`Error sending password reset email: ${error}`);
     }
 }
