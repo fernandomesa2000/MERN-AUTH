d340b33399ea35c891cde0b19f2bafabb723a090
Fernando Bedoya <juanfer30082000@gmail.com>
2026-07-05 12:52:08 -0500

    frontend login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
 import FloatingShape from "./components/FloatingShape"
 import { Navigate, Route, Routes } from "react-router-dom"
 
-import SignUpPage from "./Pages/SignUpPage"
-import LoginPage from "./Pages/LoginPage"
-import EmailVerificationPage from "./Pages/EmailVerificationPage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import EmailVerificationPage from "./pages/EmailVerificationPage"
+import DashboardPage from "./pages/DashboardPage"
 
 import { Toaster } from "react-hot-toast"
 import { useAuthStore } from "./store/authStore"
 import { useEffect } from "react"
 
-//Redirect authenticated user to homepage
+// Protects routes that require authentication
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+
+  return children;
+}
+
+// Redirect authenticated user to homepage
 
 const RedirectAuthenticatedUser = ({ children }) => {
   const { isAuthenticated, user } = useAuthStore();
@@ -38,7 +55,13 @@ function App() {
       <FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
       <FloatingShape color='bg-green-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
       <Routes>
-        <Route path="/" element={"Home"} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>}
+        />
         <Route
           path="/signup"
           element={
@@ -47,12 +70,17 @@ function App() {
             </RedirectAuthenticatedUser>
           }
         />
-        <Route path="/login" element={
-          <RedirectAuthenticatedUser>
-            <LoginPage />
-          </RedirectAuthenticatedUser>
-        } />
-        <Route path="/verify-email" element={<EmailVerificationPage />} />
+        <Route path="/login"
+          element={
+            <RedirectAuthenticatedUser>
+              <LoginPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/verify-email"
+          element={<EmailVerificationPage />}
+        />
       </Routes>
       <Toaster />
     </div >
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -16,13 +16,34 @@ export const useAuthStore = create((set) => ({
         set({ isLoading: true, error: null });
         try {
             const response = await axios.post(`${API_URL}/signup`, { email, password, name });
-            set({ user: response.data.user, isAuthenticated: true, isLoading: false });
+            set({
+                user: response.data.user,
+                isAuthenticated: true,
+                isLoading: false
+            });
         } catch (error) {
-            set({ error: error.response.data.message || "Error signing up", isLoading: false });
+            set({
+                error: error.response.data.message || "Error signing up", isLoading: false
+            });
             throw error;
         }
     },
 
+    login: async (email, password) => {
+        set({ isLoading: true, error: null });
+        try {
+            const response = await axios.post(`${API_URL}/login`, { email, password });
+            set({
+                isAuthenticated: true,
+                user: response.data.user,
+                error: null,
+                isLoading: false
+            });
+        } catch (error) {
+            set({ error: error.response?.data?.message || "Error logging in", isLoading: false });
+            throw error;
+        }
+    },
     verifyEmail: async (code) => {
         set({ isLoading: true, error: null });
         try {
