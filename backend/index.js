0c076727bf02b15ab3dee536a27e3e082efd0d62
Fernando Bedoya <juanfer30082000@gmail.com>
2026-07-09 20:08:56 -0500

    Finished Project

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -11,13 +12,25 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: process.env.NODE_ENV === "production"
+        ? true // or production real domain
+        : "http://localhost:5173",
+}));
 
 app.use(express.json()); // allows us to parse incoming requests: req.body
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("/*splat", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 
 app.listen(PORT, () => {
